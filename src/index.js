cb6f9cbe707af106bed3d80e3a417b8ecf62774d
Stepan <kurakste@gmail.com>
2019-03-17 19:25:24 +0300

    remove unused files. Add folder structure.

--- src/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-function greet(person) {
-    return "Hello, " + person.firstName;
-}
-var user = { firstName: 'Helen', lastName: 'Hreber' };
-console.log(greet(user));
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
-import { Person } from './interfaces/person'
+import express from 'express';
 
-function greet(person: Person) {
-    return "Hello, " + person.firstName;
-}
+const app = express();
+const port = 3000;
 
-const user = { firstName: 'Helen', lastName: 'Hreber' };
-console.log(greet(user));
+app.get('/', (req, res) => {
+    console.log('connect');
+    res.send('hello world!');
+});
+
+app.listen(port, ()=> {
+    console.log(`Server started on port ${port}`);
+})
