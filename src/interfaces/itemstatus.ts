0325b95ddbef9109a5a6ec32198e8f70e8c95058
Stepan <kurakste@gmail.com>
2019-03-20 18:57:48 +0300

    Add items model

---
+++ src/interfaces/itemstatus.ts
@@ -0,0 +1,9 @@
+enum  ItemStatus {
+  newitem = 0,
+  tested = 1,
+  rjected = 3,
+  approved = 4, 
+  decline = 5 
+};
+
+export default ItemStatus;
---
+++ src/model/items.ts
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+import Istatus from '../interfaces/itemstatus';
+
+const itemsSchema = new mongoose.Schema({
+  _id: mongoose.Types.ObjectId,
+  name: { type: String, reqierd: true },
+  indentifer: { 
+      type: String, 
+      reqierd: true, 
+      unique: true
+  },
+  link1: { type: String, reqierd: false },
+  link2: { type: String, reqierd: false },
+  roi: { type: String, reqierd: false },
+  created_at: { type: Date, reqierd: true },
+  checkedstate: { type: Number, reqierd: false },
+  chekedby: { type: mongoose.Types.ObjectId, reqierd: false },
+  checkednotes: { type: String, reqierd: false },
+  checked_at: { type: Date, reqierd: true },
+  status: { 
+      type: Number, 
+      reqierd: false,
+      default: Istatus.newitem
+    },
+  paid_at: { type: Date, reqierd: false },
+});
+
+export default mongoose.model('items', itemsSchema);
