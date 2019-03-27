4c61ca60f310ef4cda5669eb3b3b79587389b0f0
Stepan <kurakste@gmail.com>
2019-03-27 12:55:42 +0300

    Add patch item method.

--- src/controllers/ManagerController.ts
+++ src/controllers/ManagerController.ts
@@ -1,6 +1,7 @@
 import Item from '../model/item';
 import mongoose from 'mongoose';
 import getCurrentUser from '../helpers/getCurrentUser';
+import ItemStatus from '../interfaces/itemstatus';
 
 const controller = {
   /**
@@ -9,6 +10,7 @@ const controller = {
    *  */ 
   post_add_items: (req: any, res: any, next: Function):void => {
     const user = getCurrentUser(req); 
+    console.log(req.body);
     const { name, link1, link2, id, roi } = req.body;
     if (!(name && id && roi)) return res.status(406).json({
       message: "name, id, roi - fields is required"
@@ -26,18 +28,48 @@ const controller = {
         res.status(200).json(data);
       })
       .catch(err => {
+        console.log(err);
         res.status(500).json({
           error: err
         });
       });
   },
+
+  patch_item: (req: any, res: any, next: Function):void => {
+    //const user = getCurrentUser(req); 
+    const {_id, name, link1, link2, id, roi } = req.body;
+
+    Item.findOne({_id: _id })
+      .then((item: any) => {
+        item.name = name;
+        item.link1 = link1;
+        item.link2 = link2;
+        item.id = id;
+        item.roi = parseFloat(roi);
+        item.save()
+          .then((result: any) =>{
+              return res.status(200).json(result);
+            });
+          })
+          .catch(err => {
+            return res.status(500).json({
+              error: err
+            });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  },
+
   get_all_items: (req: any, res: any, next: Function):void => {
     const user = getCurrentUser(req); 
     Item.find({
       createdby: user.userId  
     })
+    .populate('users')
     .exec()
     .then(items => {
+      console.log('items: ', items);
       return res.status(200).json(items);
     })
     .catch(err => {
@@ -47,9 +79,10 @@ const controller = {
     });
   },
   get_check_item: (req: any, res: any, next: Function):void => {
-    const itemid = req.body.iid;
+    const itemid = req.query.iid;
+    console.log(itemid);
     Item.find({
-      _id: itemid  
+      id: itemid  
     })
     .exec()
     .then(items => {
--- src/routers/managersRoute.ts
+++ src/routers/managersRoute.ts
@@ -3,6 +3,7 @@ const router = express.Router();
 import ManagerController from '../controllers/ManagerController';
 
 router.post('/add-item', ManagerController.post_add_items);
+router.patch('/item', ManagerController.patch_item);
 router.get('/items', ManagerController.get_all_items);
 router.get('/check-item', ManagerController.get_check_item);
 
