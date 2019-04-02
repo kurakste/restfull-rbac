c0c42263102ba419198f7905c2ddfa5f7e85f4d0
Stepan <kurakste@gmail.com>
2019-04-02 07:45:17 +0300

    add OPTION request

--- src/index.ts
+++ src/index.ts
@@ -35,7 +35,17 @@ mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
 
-app.use(cors());
+app.use(cors({
+
+}));
+
+app.options("/*", function(req, res, next){
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    res.send(200);
+  });
+
 app.use(bodyParser.json());
 app.use(auth);
 
