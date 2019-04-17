cbde0d3b4763677b5c35fda30dabde4e868c0a87
Stepan <kurakste@gmail.com>
2019-04-17 17:30:44 +0300

    fix issue with images list

--- src/console/getAmazonProductDescrtiption.ts
+++ src/console/getAmazonProductDescrtiption.ts
@@ -29,7 +29,8 @@ async function asyncForEach(array:Array<any>, callback:CallableFunction) {
 const downloadimages = async (items:any) => {
   if (!Array.isArray(items)) return;
   if (items.length === 0) return ;
-  items.map((el:any) => {
+  items.map((el:string) => {
+    const elurl: string = el.replace('__', '%'); // i make this replacmente before
     imagDownloader(
         `https://images-na.ssl-images-amazon.com/images/I/${el}._SX1500_.jpg`,
         `./public/img/${el}.jpg`,
