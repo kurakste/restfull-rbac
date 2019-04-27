91286c669dc7592f97e81703198c9261c14454ab
Stepan <kurakste@gmail.com>
2019-04-27 18:24:44 +0300

    Add parsing for managers controller

--- src/routers/managersRoute.ts
+++ src/routers/managersRoute.ts
@@ -5,6 +5,8 @@ import ManagerController from '../controllers/ManagerController';
 router.post('/add-item', ManagerController.post_add_items);
 router.patch('/item', ManagerController.patch_item);
 router.get('/items', ManagerController.get_all_items);
+router.get('/parse', ManagerController.get_parse);
+router.get('/amazon-item', ManagerController.get_amazon_item);
 router.get('/check-item', ManagerController.get_check_item);
 
 export default router;
