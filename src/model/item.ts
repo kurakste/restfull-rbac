bb4fad3bf9b9331c4668cb2cb6a677e9f6d47060
Stepan <kurakste@gmail.com>
2019-05-01 20:03:16 +0300

    add niew filds in item odel

--- src/model/item.ts
+++ src/model/item.ts
@@ -3,42 +3,51 @@ import Istatus from '../interfaces/itemstatus';
 
 const itemSchema = new mongoose.Schema({
   _id: mongoose.Types.ObjectId,
-  name: { type: String, reqierd: true },
-  id: { 
-      type: String, 
-      reqierd: true, 
-      unique: true,
-      dropDups: true,
-  },
-  amazondesc: { type: mongoose.Types.ObjectId, ref: 'amazon', reqierd: false },
+  //name: { type: String, reqierd: true },
+  // Введите номер товара Asin / получаем из ссылки на амазон
+  id: { type: String, reqierd: true, unique: true, dropDups: true  },
+  //is it usfull? 
+  //amazondesc: { type: mongoose.Types.ObjectId, ref: 'amazon', reqierd: false },
+  //Link for amazon
   lamazon: { type: String, reqierd: false },
+  //Link for supplier
   lsupplier: { type: String, reqierd: false },
+  //best sales rate from amazon
   bsr: { type: Number, reqierd: false },
   fba: { type: Boolean, default: false },
+  minpurchase:{ type: Number, reqierd: false }, 
+  //Buybox price. Price from amazon.
   amazon: { type: Number, reqierd: false },
+  //Supplier price include delivery costs. 
   supplier: { type: Number, reqierd: false },
-  commission: { type: Number, reqierd: false },
+  //Referal fee, ammazon's commission. Only for no fba.
+  reffee: { type: Number, reqierd: false },
+  //Total fba fee = referal fee + fba fee). Only for fba.
+  fbafee: { type: Number, reqierd: false },
+  //Delivery cost per one ps. 
   delivery: { type: Number, reqierd: false },
+  //Calculated profit per one ps.
   profit: { type: Number, reqierd: false },
+  //Margin required only for non fba. Wish in fba.
   margin: { type: Number, reqierd: false },
-  createdat: { type: Date, reqierd: true },
+  //any comments from manager for this item.
   icomment: { type: String, reqierd: false },
+  createdat: { type: Date, reqierd: true },
   createdby: { type: mongoose.Types.ObjectId, ref: 'users', reqierd: true },
   checkedstate: { type: Number, reqierd: false },
   checkedby: { type: mongoose.Types.ObjectId, ref: 'users', reqierd: false },
   checkednotes: { type: String, reqierd: false },
   checkedat: { type: Date, reqierd: true },
-  status: { 
-      type: Number, 
-      reqierd: false,
-      default: Istatus.newitem
-    },
-  paid_at: { type: Date, reqierd: false },
+  status: { type: Number, reqierd: false,default: Istatus.newitem },
   managerFine: { type: Number, required: false},
   managerFineComment: { type: String, reqierd: false },
   supervisorFine: { type: Number, required: false},
   supervisorFineComment: { type: String, reqierd: false },
-
+  // Data whent the payment was made.
+  paidat: { type: Date, reqierd: false },
+  //Who made this payment.
+  paidby:  { type: mongoose.Types.ObjectId, ref: 'users', reqierd: false },
+  images: { type: Array },
 });
 
 export default mongoose.model('items', itemSchema);
