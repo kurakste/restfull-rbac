7ba1c6745f7546c1e0b24ba836bad9017e8b29a8
Stepan <kurakste@gmail.com>
2019-05-01 20:02:49 +0300

    some formating

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,38 +1,47 @@
-import jwt from'jsonwebtoken';
+import jwt from 'jsonwebtoken';
+import express from 'express';
 import premissions from '../routers/routePremissions';
 import _ from 'lodash';
 import apiDataObject from '../helpers/apiDataObject';
+import cl from '../helpers/debugMessageLoger';
 
-const auth = (req: any, res: any, next:any) => {
-  
+const auth = (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction) => {
+
   const path = req.path.split('/');
-  console.log('path:', path);
-  if (path[1]==='public') return next();
+  cl('auth midleware: ', ['path: ', req.path, 'method', req.method])
+  if (path[1] === 'public') return next();
 
   const prem: any = _.filter(
-    premissions, 
+    premissions,
     { path: req.path, method: req.method }
-    );
+  );
+
+  
+  if (prem.length > 1) throw new Error('We have more then one premission records for this path');
+  if (prem.length === 0) return res
+  .status(200)
+  .json(apiDataObject(null, false, 'Auth error'));
+  
+  if (prem[0].premited === 5) {
+    return next(); // This path is not protected
+  }
+  if (!process.env.JWT_KEY) throw new Error('JWT_KEY has to be set in .env file');
+  try {
+    const token =
+    (req.headers.authorization)
+    ? req.headers.authorization.split(' ')[1] : '';
     
-    if (prem.length > 1) throw new Error('We have more then one premission records for this path');
-    if (prem.length === 0) return res
-    .status(200)
-    .json(apiDataObject(null, false, 'Auth error'));
-    
-    if (prem[0].premited === 5) {
-      return next(); // This path is not protected
-    }
-    if (!process.env.JWT_KEY) throw new Error('JWT_KEY has to be set in .env file');
-    try {
-      const token = req.headers.authorization.split(' ')[1];
-      const user: any = jwt.verify(token, process.env.JWT_KEY);
-      if (user.role > prem[0].premited) return res
+    const user: any = jwt.verify(token, process.env.JWT_KEY);
+    if (user.role > prem[0].premited) return res
       .status(200)
-      .json(apiDataObject(null, false, 'Premmision denied.')) 
+      .json(apiDataObject(null, false, 'Premmision denied.'))
     return next();
   } catch (error) {
-      return res.status(200)
-      .json(apiDataObject(null, false, 'Auth error.')); 
+    return res.status(200)
+      .json(apiDataObject(null, false, 'Auth error.'));
   }
 };
 
