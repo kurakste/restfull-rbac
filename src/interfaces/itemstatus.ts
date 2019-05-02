e9d1dc5175d3e67f24c8f3fd8fde2adda5807aed
Stepan <kurakste@gmail.com>
2019-05-02 23:06:34 +0300

    change items status

--- src/interfaces/itemstatus.ts
+++ src/interfaces/itemstatus.ts
@@ -2,8 +2,11 @@ enum  ItemStatus {
   newitem = 0,
   tested = 1,
   rjected = 2,
-  approved = 3, 
-  decline = 4 
+  forfba = 3,
+  fbadone = 4,
+  fbaok = 5,
+  approved = 6, 
+  decline = 7 
 };
 
 export default ItemStatus;
