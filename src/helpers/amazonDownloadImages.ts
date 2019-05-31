d7934c2fce5a7172f9d4336bbbd11e6b0d54a3bd
Stepan <kurakste@gmail.com>
2019-05-31 22:56:44 +0300

    fix problem with filename

--- src/helpers/amazonDownloadImages.ts
+++ src/helpers/amazonDownloadImages.ts
@@ -5,7 +5,7 @@ const downloadimages = async (items: any) => {
   if (!Array.isArray(items)) return;
   if (items.length === 0) return;
   const prombundle = items.map(async (el: string) => {
-    const url:string = el.replace(/_/g, '%'); // can't use % to store in fs.
+    const url:string = el.replace(/%/g, '_'); // can't use % to store in fs.
     return imagDownloader(
       `https://images-na.ssl-images-amazon.com/images/I/${url}._SX1500_.jpg`,
       `./public/img/${url}.jpg`,
